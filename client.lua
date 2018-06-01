e768498e417b3aaf46d651c25584d8bfb3a2f5e6
Thaisen69 <39800430+Thaisen69@users.noreply.github.com>
2018-06-01 13:13:38 +0200

    Update client.lua
    Added veh check

--- client.lua
+++ client.lua
@@ -32,7 +32,11 @@ Citizen.CreateThread(function()
 		local speed = GetEntitySpeed(vehicle)
 		local kmh = 3.6
 		local mph = 2.23694
-		
+		local vehicleClass = GetVehicleClass(vehicle)
+		local vehicleModel = GetEntityModel(vehicle)
+			
+			
+		if vehicleClass ~= 15 and 16 then
 		GetEntitySpeed(GetPedInVehicleSeat(GetPlayerPed(-1), false)) 
 		-- If you want mph, then replace kmh with mph under here. If you want more or less than 30 also change it here
 		if math.floor(speed*kmh) > 30 then
@@ -43,4 +47,30 @@ Citizen.CreateThread(function()
 				SetPlayerCanDoDriveBy(PlayerId(), false)
 			end
 		end
+	end
 end)
+
+
+
+
+-- VEHICLE CLASSES
+-- id = 0 --compacts
+-- id = 1 --sedans
+-- id = 2 --SUV's
+-- id = 3, --coupes
+-- id = 4 --muscle
+-- id = 5 --sport classic
+-- id = 6 --sport
+-- id = 7 --super
+-- id = 8 --motorcycle
+-- id = 9 --offroad
+-- id = 10 --industrial
+-- id = 11 -utility
+-- id = 12 --vans
+-- id = 13 --bicycles
+-- id = 14 --boats
+-- id = 15, --helicopter
+-- id = 16 --plane
+-- id = 17 --service
+-- id = 18 --emergency
+-- id = 19 --military
